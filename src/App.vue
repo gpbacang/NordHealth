f9f958bdb12fba7c9ee0f716b2dac293a9ca2f05
Geraldine Bacang <geraldine.bacang@gmail.com>
2025-03-09 15:00:33 +0100

    Use layouts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import AppLayout from '@/layouts/AppLayout.vue'
+import AuthorizationLayout from '@/layouts/AuthorizationLayout.vue'
+
+const route = useRoute()
+
+const isProtectedRoute = computed(() => route.meta.requiresAuth)
 </script>
 <template>
-  <!-- <provet-stack class="n-reset"> -->
-  <div class="n-padding-xxl">
-    <provet-card style="height: 100%">
-      <section style="height: 100%">
-        <!-- <div
-          class="n-color-accent n-border-radius n-color-text-on-accent n-typeset"
-          style="height: 100%"
-        >
-          <div class="n-padding-m">
-            <h1 class="n-color-text-on-accent">Vue App</h1>
-            <div>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
-              provident aliquid corporis tempora eius quam, quae vel molestias
-              esse maxime aperiam totam suscipit. Nobis consectetur optio
-              reprehenderit animi? Suscipit, quibusdam?
-            </div>
-          </div>
-        </div> -->
-        <div>
-          <RouterView />
-        </div>
-      </section>
-    </provet-card>
-  </div>
-  <!-- </provet-stack> -->
+  <AppLayout v-if="isProtectedRoute" />
+  <AuthorizationLayout v-else />
 </template>
