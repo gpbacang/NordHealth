b55262826277c0466b10dbdb7799255668dbd3f0
Geraldine Bacang <geraldine.bacang@gmail.com>
2025-03-09 15:04:49 +0100

    Create layouts

---
+++ src/layouts/AuthorizationLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+<template>
+  <div class="n-padding-xxl">
+    <provet-card class="100vh">
+      <section class="h-full">
+        <main>
+          <RouterView />
+        </main>
+      </section>
+    </provet-card>
+  </div>
+</template>
