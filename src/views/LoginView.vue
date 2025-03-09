7d06b6bdef9f35b3a0628e33e33e77278e1a4523
Geraldine Bacang <geraldine.bacang@gmail.com>
2025-03-09 18:21:45 +0100

    Add UI for login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,2 +1,24 @@
-<script setup lang="ts"></script>
-<template>login view!</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import LoginForm from '@/components/LoginForm.vue'
+
+const router = useRouter()
+
+const routeToRegistration = () => {
+  router.push({ name: 'register' })
+}
+</script>
+
+<template>
+  <div class="n-typeset p-10">
+    <h1>Login</h1>
+    <div class="flex items-center mt-4 mb-10">
+      <div>Don't have an account yet?</div>
+      <div class="underline ml-1 cursor-pointer" @click="routeToRegistration">
+        Create an account
+      </div>
+    </div>
+    <LoginForm />
+  </div>
+</template>
