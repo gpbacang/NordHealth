b0ffee7161842f446843393cb24d5c8450408940
Geraldine Bacang <geraldine.bacang@gmail.com>
2025-03-09 15:05:51 +0100

    Add fields to account in the store

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -4,11 +4,20 @@ import { ref } from 'vue'
 import type { Account } from '@/types/Account'
 
 export const useAccountStore = defineStore('account', () => {
-  const account = ref<Account>()
+  const account = ref<Account>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    receiveUpdates: false,
+  })
 
   const saveAccount = (data: Account) => {
     account.value = data
   }
 
-  return { account, saveAccount }
+  return {
+    account,
+    saveAccount,
+  }
 })
