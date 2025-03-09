231dd5534b1e3627d08477b802dd6da5f8564dda
Geraldine Bacang <geraldine.bacang@gmail.com>
2025-03-09 18:21:14 +0100

    Add types

--- src/types/Account.ts
+++ src/types/Account.ts
@@ -1,7 +1,15 @@
-export type Account = {
-  firstName: string
-  lastName: string
-  email: string
-  password: string
+export type AccountData = {
+  firstName?: string
+  lastName?: string
+  email?: string
+  password?: string
+  confirmPassword?: string
   receiveUpdates?: boolean
 }
+
+export type LoginData = Omit<
+  AccountData,
+  'firstName' | 'lastName' | 'confirmPassword' | 'receiveUpdates'
+>
+
+export type AccountUpdateData = Omit<AccountData, 'password' | 'confirmPassword' | 'receiveUpdates'>
